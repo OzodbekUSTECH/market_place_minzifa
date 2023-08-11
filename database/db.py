435023512bc9198c3dcf6d7a82b0333ea9f14e20
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-08-12 00:12:45 +0500

    deploy

--- database/db.py
+++ database/db.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+# DATABASE_URL = "postgresql://postgres:77girado@db:5432/marketplacedb" develop
 
-DATABASE_URL = "postgresql://postgres:77girado@db/marketplacedb"
+DATABASE_URL = "postgresql://postgres:77girado@db:5432/marketplacedb"
 
 engine = create_engine(DATABASE_URL, echo=True)
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
