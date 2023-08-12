708ab5e7c9a6e6d4545beb4abf55494a0603dc92
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-08-12 11:33:08 +0500

    permissions for rolepermissions router

--- api/routers/rolepermissions.py
+++ api/routers/rolepermissions.py
@@ -4,7 +4,7 @@ from services.rolepermissions import RolePermissionsService
 from utils.dependency import get_rolepermissions, get_current_user
 from repositories.base import Pagination
 from schemas.rolepermissions import RolePermissionsSchema, CreateRolePermissionsSchema, DeleteRolePermissionsSchema
-
+from utils.permissions import give_role_permission, delete_role_permission
 router = APIRouter(
     prefix="/rolepermissions",
     tags=["Role and Permission connection"],
@@ -12,7 +12,7 @@ router = APIRouter(
 
 
 
-@router.post('', name='Give permission for a role', response_model=RolePermissionsSchema)
+@router.post('', name='Give permission for a role', response_model=RolePermissionsSchema, dependencies=[Depends(give_role_permission)])
 async def give_permission_data_for_role(
     role_permission_data: CreateRolePermissionsSchema,
     roleperms_service: Annotated[RolePermissionsService, Depends(get_rolepermissions)]
@@ -20,7 +20,7 @@ async def give_permission_data_for_role(
     return await roleperms_service.give_permission_for_role(role_permission_data)
 
 
-@router.delete('/{role_id}/{permission_id}', name='Give permission for a role', response_model=RolePermissionsSchema)
+@router.delete('/{role_id}/{permission_id}', name='Give permission for a role', response_model=RolePermissionsSchema, dependencies=[Depends(delete_role_permission)])
 async def give_permission_data_for_role(
     role_permission_data: DeleteRolePermissionsSchema,
     roleperms_service: Annotated[RolePermissionsService, Depends(get_rolepermissions)]
