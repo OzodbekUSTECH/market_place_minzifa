b154915d086db51907d07d8343b8efee3ed521d5
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-08-12 11:28:20 +0500

    gave permissions for roles routers

--- api/routers/roles.py
+++ api/routers/roles.py
@@ -4,20 +4,21 @@ from services.roles import RolesService
 from utils.dependency import get_rolesservices, get_current_user
 from repositories.base import Pagination
 from schemas.roles import RoleSchema, CreateRoleSchema, UpdateRoleSchema, RolePermissionsSchema
+from utils.permissions import read_roles, read_role, read_role_permissions, create_role, update_role, delete_role
 router = APIRouter(
     prefix="/roles",
     tags=["Roles"],
 )
 
 
-@router.get('', name="get list of roles", response_model=list[RoleSchema])
+@router.get('', name="get list of roles", response_model=list[RoleSchema], dependencies=[Depends(read_roles)])
 async def get_list_of_roles(
     pagination: Annotated[Pagination, Depends()],
     roles_service: Annotated[RolesService, Depends(get_rolesservices)]
 ) -> list[RoleSchema]:
     return await roles_service.get_all_roles(pagination)
 
-@router.get('/{role_id}/permissions', name="get all permissions of role", response_model=list[RolePermissionsSchema])
+@router.get('/{role_id}/permissions', name="get all permissions of role", response_model=list[RolePermissionsSchema], dependencies=[Depends(read_role_permissions)])
 async def get_permissions(
     role_id: int,
     roles_service: Annotated[RolesService, Depends(get_rolesservices)]
@@ -26,21 +27,21 @@ async def get_permissions(
 
 
 
-@router.get('/{role_id}', name="get role by ID", response_model=RoleSchema)
+@router.get('/{role_id}', name="get role by ID", response_model=RoleSchema, dependencies=[Depends(read_role)])
 async def get_role_data_by_id(
     role_id: int,
     roles_service: Annotated[RolesService, Depends(get_rolesservices)]
 ) -> RoleSchema:
     return await roles_service.get_role_by_id(role_id)
 
-@router.post('', name="Create Role", response_model=RoleSchema)
+@router.post('', name="Create Role", response_model=RoleSchema, dependencies=[Depends(create_role)])
 async def create_role_data(
     role_data: CreateRoleSchema,
     roles_service: Annotated[RolesService, Depends(get_rolesservices)]
 ) -> RoleSchema:
     return await roles_service.create_role(role_data)
 
-@router.put('/{role_id}', name="Update Role", response_model=RoleSchema)
+@router.put('/{role_id}', name="Update Role", response_model=RoleSchema,dependencies=[Depends(update_role)])
 async def get_role_data_by_id(
     role_id: int,
     role_data: UpdateRoleSchema,
@@ -49,7 +50,7 @@ async def get_role_data_by_id(
     return await roles_service.update_role(role_id, role_data)
 
 
-@router.delete('/{role_id}', name="Create Role", response_model=RoleSchema)
+@router.delete('/{role_id}', name="Create Role", response_model=RoleSchema, dependencies=[Depends(delete_role)])
 async def get_role_data_by_id(
     role_id: int,
     roles_service: Annotated[RolesService, Depends(get_rolesservices)]
