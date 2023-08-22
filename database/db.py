34f3dbd715eb2d0857f29617459a152ffd3aaf9e
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-08-22 18:03:39 +0500

    fixing connection to db

--- database/db.py
+++ database/db.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 # DATABASE_URL = "postgresql://postgres:77girado@localhost:5432/marketplacedb"
 
 #production
-DATABASE_URL = "postgresql://postgres:77girado@localhost:5432/postgres"
+DATABASE_URL = "postgresql://postgres:77girado@db/postgres"
 
 engine = create_engine(DATABASE_URL, echo=True)
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
