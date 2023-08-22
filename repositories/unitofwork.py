318ab2cf01084fe6d6f9e88905f6d427d70ad524
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-08-22 18:08:41 +0500

    sync db

--- repositories/unitofwork.py
+++ repositories/unitofwork.py
@@ -35,12 +35,12 @@ class UnitOfWork:
         self.travelers_managers = TravelerManagersRepository(self.session, model=TravelersAndManagersAssociation)
 
     async def __aexit__(self, *args):
-        await self.rollback()
-        await self.session.close()
+        self.rollback()
+        self.session.close()
 
     async def commit(self):
-        await self.session.commit()
+        self.session.commit()
 
     async def rollback(self):
-        await self.session.rollback()
+        self.session.rollback()
         
