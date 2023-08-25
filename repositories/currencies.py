07300745c5a86b7a0ca10f9b8ea730fec0582d8d
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-08-25 18:15:14 +0500

    bugs

--- repositories/currencies.py
+++ repositories/currencies.py
@@ -5,6 +5,8 @@ from sqlalchemy import func
 
 class CurrenciesRepository(BaseRepository):    
     async def get_by_name(self, name: str):
-        return self.session.query(self.model).filter(func.lower(self.model.name) == name.lower()).first()
+        currencies = self.session.query(self.model).filter(func.lower(self.model.name) == name.lower()).first()
+        self.session.commit()
+        return currencies
     
     
--- repositories/tourprices.py
+++ repositories/tourprices.py
@@ -2,7 +2,9 @@ from repositories import BaseRepository
 
 class TourPricesRepository(BaseRepository):
     async def get_by_tour_id(self, tour_id):
-        return self.session.query(self.model).filter(self.model.tour_id == tour_id).all()
+        prices = self.session.query(self.model).filter(self.model.tour_id == tour_id).all()
+        self.session.commit()
+        return prices
         
     
 
