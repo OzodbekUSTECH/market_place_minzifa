4e7a981f494c49eaefe3cf5ccded5b5c4b5f353d
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-08-25 11:33:47 +0500

    routers

--- api/__init__.py
+++ api/__init__.py
@@ -4,11 +4,15 @@ from api.routers.roles import router as roles_router
 from api.routers.permissions import router as permissions_router
 from api.routers.rolepermissions import router as rolepermissions_router
 from api.routers.travelermanagers import router as travelermanagers_router
+from api.routers.currencies import router as currency_router
+from api.routers.tours import router as tour_router
 all_routers = [
     auth_router,
     maillist_router,
     roles_router,
     permissions_router,
     rolepermissions_router,
-    travelermanagers_router
+    travelermanagers_router,
+    currency_router,
+    tour_router
 ]
