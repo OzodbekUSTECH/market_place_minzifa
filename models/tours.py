e30d91ac28461e9a9e1f6fd215751b1698ac7b60
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-08-25 18:09:35 +0500

    fixed

--- models/tours.py
+++ models/tours.py
@@ -11,14 +11,4 @@ class Tour(Base):
     prices = relationship("TourPrice", back_populates="tour", lazy="subquery")
 
 
-    
-class TourPrice(Base):
-    __tablename__ = 'tour_prices'
-    
-    id = Column(Integer, primary_key=True)
-    tour_id = Column(Integer, ForeignKey('tours.id'))
-    currency_id = Column(Integer, ForeignKey('currencies.id'))
-    price = Column(Float, nullable=False)
-    
-    tour = relationship("Tour", back_populates="prices", lazy="subquery")
-    currency = relationship("Currency", lazy="subquery")
+
