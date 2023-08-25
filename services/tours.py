e47a4181426e7b3d9fd072400a3621da2a0cd1c9
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-08-25 17:10:28 +0500

    fixing error

--- services/tours.py
+++ services/tours.py
@@ -10,7 +10,7 @@ class ToursService:
         self.uow = uow
 
     # Ваш метод для создания тура с ценами
-    async def create_tour(self, tour_data: CreateTourSchema):
+    async def create_tour(self, tour_data: CreateTourSchema) -> TourSchema:
         tour_dict = tour_data.model_dump(exclude={"price"})
         async with self.uow:
             created_tour = await self.uow.tours.create(tour_dict)
@@ -19,27 +19,30 @@ class ToursService:
                 price=tour_data.price
             )
             #had to use get_by_id for the response model in the router! Otherwise it doesn't work
+            #i got a fastapi.exceptions.ResponseValidationError
             # tour = await self.uow.tours.get_by_id(created_tour.id)
             return created_tour
 
     async def _create_prices_for_tour(self, tour: Tour, price: float):
-        async with self.uow:
-            base_currency = await self.uow.currencies.get_by_name('USD')
-            target_currencies = await self.uow.currencies.get_all()        
-            for target_currency in target_currencies:
-                converted_price = price if target_currency == base_currency else price * target_currency.exchange_rate
-                
-                create_price_data = CreateTourPriceSchema(
-                    tour_id=tour.id,
-                    currency_id=target_currency.id,
-                    price=converted_price
-                )
-                await self.uow.tour_prices.create(create_price_data.model_dump())
+        base_currency = await self.uow.currencies.get_by_name('USD')
+        target_currencies = await self.uow.currencies.get_all()        
+        for target_currency in target_currencies:
+            converted_price = price if target_currency == base_currency else price * target_currency.exchange_rate
+            
+            create_price_data = CreateTourPriceSchema(
+                tour_id=tour.id,
+                currency_id=target_currency.id,
+                price=converted_price
+            )
+            await self.uow.tour_prices.create(create_price_data.model_dump())
         
             
 
-    async def get_list_of_tours(self, pagination: Pagination):
+    async def get_list_of_tours(self, pagination: Pagination) -> TourSchema:
         async with self.uow:
             list_of_tours = await self.uow.tours.get_all(pagination)
-            # await self.uow.commit()
             return list_of_tours
+        
+    async def get_tour_by_id(self, tour_id: int) -> TourSchema:
+        async with self.uow:
+            return await self.uow.tours.get_by_id(tour_id)
