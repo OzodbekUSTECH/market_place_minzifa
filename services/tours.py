56a8b95a5b96d0179dfb86937b5353280da416d2
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-08-25 16:56:07 +0500

    fixing error

--- services/tours.py
+++ services/tours.py
@@ -13,20 +13,16 @@ class ToursService:
     async def create_tour(self, tour_data: CreateTourSchema):
         async with self.uow:
             created_tour = await self.uow.tours.create({"name": tour_data.name})
-            prices = await self._create_prices_for_tour(
+            await self._create_prices_for_tour(
                 tour_id=created_tour.id,
                 price=tour_data.price
             )
-            if prices:
-                tour = await self.uow.tours.get_by_id(created_tour.id)
-                return tour
+            tour = await self.uow.tours.get_by_id(created_tour.id)
+            return tour
 
     async def _create_prices_for_tour(self, tour_id: int, price: float):
         base_currency = await self.uow.currencies.get_by_name('USD')
-        target_currencies = await self.uow.currencies.get_all()
-        
-        # prices_to_create = []
-        
+        target_currencies = await self.uow.currencies.get_all()        
         for target_currency in target_currencies:
             converted_price = price if target_currency == base_currency else price * target_currency.exchange_rate
             
@@ -36,9 +32,7 @@ class ToursService:
                 price=converted_price
             )
             await self.uow.tour_prices.create(create_price_data.model_dump())
-            # prices_to_create.append(TourPriceSchema(**created_price.__dict__))
         
-        return True
             
 
     async def get_list_of_tours(self, pagination: Pagination):
