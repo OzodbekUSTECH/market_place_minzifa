68fcfe891ade26c63f6e034daeef6b306b67ac2b
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-08-27 18:41:48 +0500

    debugging eror

--- repositories/tours.py
+++ repositories/tours.py
@@ -14,7 +14,6 @@ class ToursRepository(BaseRepository):
                     
                 if not query or fuzz.partial_ratio(query.lower(), tour.title.lower()) > 60:
 
-                    if not tour_rating or tour.tour_rating == tour_rating:
 
                         matched_tours.append(tour)
 
