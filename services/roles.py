d2d3368d86b46bea2a64da81466fef5425f5e14b
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-08-28 16:44:07 +0500

    correct responses

--- services/roles.py
+++ services/roles.py
@@ -55,4 +55,4 @@ class RolesService:
         async with self.uow:
             role = await self.uow.roles.get_by_id(role_id)
             
-            return [perm.permission.to_read_model() for perm in role.role_permissions]
+            return role.role_permissions
