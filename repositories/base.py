043c710b02b46d3991cec9d2952a35e155390f71
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-09-04 16:16:45 +0500

    improving code with filters for tours

--- repositories/base.py
+++ repositories/base.py
@@ -2,6 +2,8 @@ from sqlalchemy.orm import Session
 from sqlalchemy.ext.declarative import DeclarativeMeta
 from fastapi import Query
 from datetime import date
+from fuzzywuzzy import fuzz, process
+
 class Pagination:
     def __init__(self, page: int = Query(1, ge=1), page_size: int = Query(100, le=100)):
         self.page = page
@@ -38,6 +40,35 @@ class FilterTours:
         self.currency_id = currency_id
         self.price = price
 
+    def filter_tour(self, tour):
+        if self.status_id and tour.status_id != self.status_id:
+            return False
+
+        if self.query and fuzz.partial_ratio(self.query.lower(), tour.title.lower()) <= 60:
+            return False
+
+        if self.tour_rating and (tour.user.rating < self.tour_rating or tour.user.rating >= self.tour_rating + 0.5):
+            return False
+
+        if self.start_date and tour.start_date != self.start_date:
+            return False
+
+        if self.end_date and tour.end_date != self.end_date:
+            return False
+
+        if self.country and tour.country != self.country:
+            return False
+
+        if self.region and tour.region != self.region:
+            return False
+
+        if self.currency_id is not None and self.price is not None:
+            for tour_price in tour.prices:
+                if tour_price.currency_id == self.currency_id and tour_price.price >= self.price:
+                    return True
+            return False
+
+        return True
 class BaseRepository:
     def __init__(self, session: Session, model:DeclarativeMeta):
         self.session = session
