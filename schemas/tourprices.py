5b9f1758411c49fa3259068aeadd5ca3f93acd7a
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-09-05 15:40:37 +0500

    fixed bug

--- schemas/tourprices.py
+++ schemas/tourprices.py
@@ -9,19 +9,25 @@ class CreateTourPriceSchema(BaseModel):
     discount_percentage: float = None
     new_price: float = None
 
-    @validator('price', pre=True, always=True)
-    def round_price(cls, value):
-        return int(round(value))
     
     
     
-class UpdateTourPriceSchema(CreateTourPriceSchema):
-    pass
+    
+class UpdateTourPriceSchema(BaseModel):
+    currency_id: int
+    price: float
+    discount_percentage: float = None
+    new_price: float = None
+
 
 class TourPriceSchema(CreateTourPriceSchema):
     id: int
     currency_id: int
 
+    @validator('price', pre=True, always=True)
+    def round_price(cls, value):
+        return int(round(value))
+    
     @validator('new_price', pre=True, always=True)
     def round_price(cls, value):
         return int(round(value))
