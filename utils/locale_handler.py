fc57b4e78323a51be812aef824b39316b4ea3f6e
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-10-17 01:32:57 +0500

    updated locale handler

--- utils/locale_handler.py
+++ utils/locale_handler.py
@@ -35,13 +35,12 @@ class LocaleHandler:
                         localized_value = value.get(locale.get_language, None)
                         if localized_value is not None:
                             setattr(attr_value, key, localized_value)
-                        else:
-                            # if isinstance(value, BaseModel):
-                                for key2, value2 in value.__dict__.items():
-                                    if isinstance(value2, dict):
-                                        localized_value2 = value2.get(locale.get_language, None)
-                                        if localized_value2 is not None:
-                                            setattr(value, key2, localized_value2)
+                    elif isinstance(value, BaseModel):
+                        for key2, value2 in value.__dict__.items():
+                            if isinstance(value2, dict):
+                                localized_value2 = value2.get(locale.get_language, None)
+                                if localized_value2 is not None:
+                                    setattr(value, key2, localized_value2)
 
             # Process a list of models
             if isinstance(attr_value, list):
@@ -52,13 +51,12 @@ class LocaleHandler:
                                 sub_localized_value = sub_attr_value.get(locale.get_language, None)
                                 if sub_localized_value is not None:
                                     setattr(sub_model, sub_attr_name, sub_localized_value)
-                                else:
-                                    # if isinstance(value, BaseModel):
-                                        for key2, value2 in value.__dict__.items():
-                                            if isinstance(value2, dict):
-                                                localized_value2 = value2.get(locale.get_language, None)
-                                                if localized_value2 is not None:
-                                                    setattr(value, key2, localized_value2)
+                            elif isinstance(value, BaseModel):
+                                for key2, value2 in value.__dict__.items():
+                                    if isinstance(value2, dict):
+                                        localized_value2 = value2.get(locale.get_language, None)
+                                        if localized_value2 is not None:
+                                            setattr(value, key2, localized_value2)
 
                 return attr_value
 
