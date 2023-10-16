9fe8ed1c379643a7ce66449116b7718e16d20054
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-10-16 22:52:41 +0500

    updated filter tours

--- utils/filters/filter_tours.py
+++ utils/filters/filter_tours.py
@@ -3,39 +3,38 @@ from fuzzywuzzy import fuzz
 import models
 from utils.locale_handler import LocaleHandler
 from utils.filters.base import BaseFilterParams
-from typing import Union
-def return_none():
-    return None
+
 
 class FilterToursParams(BaseFilterParams):
     def __init__(
         self,
-        country_id:  str| None  = Query(None),
-        region_id: str | None= Query(None),
-        start_month: str | None= Query(None),
-        query: str | None= Query(None),
-        user_id: str | None= Query(None),
-        rating: str | None = Query(None),
-        status_id: str | None= Query(None),
-        duration_from: str | None = Query(None),
-        duration_to: str | None = Query(None),
-        category_id: str | None = Query(None),
-        type_id: str | None = Query(None),
-        accommodation_id: str | None = Query(None),
-        age_group_from: str | None = Query(None),
-        age_group_to: str | None = Query(None),
+        country_id: int = Query(0),
+        region_id: int = Query(0),
+        start_month: int = Query(0),
+        query: str = Query(0),
+        user_id: int = Query(0),
+        rating: int | float = Query(0),
+        status_id: int = Query(0),
+        duration_from: str = Query(0),
+        duration_to: str = Query(0),
+        category_id: int = Query(0),
+        type_id: int = Query(0),
+        accommodation_id: int = Query(0),
+        age_group_from: int = Query(0),
+        age_group_to: int = Query(0),
         is_one_day_tour: bool = Query(False),
-        children_age_id: str | None = Query(None),
-        activity_level_id: str | None = Query(None),
-        language_id: str | None = Query(None),
+        children_age_id: int = Query(0),
+        activity_level_id: int = Query(0),
+        language_id: int = Query(0),
 
         is_price_for_day: bool = Query(False),
-        currency_id: str | None = Query(None),
+        currency_id: int = Query(0),
 
-        price_from: str | None = Query(None),
-        price_to: str | None = Query(None),
+        price_from: float = Query(0),
+        price_to: float = Query(0),
 
         only_with_discounts: bool = Query(False),
+
     ):
         self.country_id = country_id
         self.region_id = region_id
@@ -64,20 +63,21 @@ class FilterToursParams(BaseFilterParams):
         self.only_with_discounts = only_with_discounts
 
 
+
     def filter_item(self, tour: models.Tour, locale: LocaleHandler):
         # Список условий фильтрации
         filters = []
 
         #проверка параметра country_id
-        if self.country_id is not None:
-            filters.append(int(self.country_id) in tour.country_ids)
+        if self.country_id:
+            filters.append(self.country_id in tour.country_ids)
 
-        if self.region_id is not None:
-            filters.append(int(self.region_id) in tour.region_ids)
+        if self.region_id:
+            filters.append(self.region_id in tour.region_ids)
 
         #проверка параметра start_month
-        if self.start_month is not None:
-            filters.append(int(self.start_month) == tour.start_month)
+        if self.start_month:
+            filters.append(self.start_month == tour.start_month)
 
         # Проверка параметра query
         if self.query:
@@ -87,70 +87,70 @@ class FilterToursParams(BaseFilterParams):
                 ) >= 60
             )
 
-        if self.user_id is not None:
-            filters.append(int(tour.user_id) == self.user_id)
+        if self.user_id:
+            filters.append(tour.user_id == self.user_id)
 
-        if self.rating is not None:
-            filters.append(float(self.rating) == tour.user.rating)
+        if self.rating:
+            filters.append(self.rating == tour.user.rating)
 
         # Проверка параметра status_id
-        if self.status_id is not None:
-            filters.append(int(self.status_id) == tour.status_id)
+        if self.status_id:
+            filters.append(tour.status_id == self.status_id)
 
         # Проверка параметра duration_from
-        if self.duration_from is not None:
-            filters.append(tour.duration >= int(self.duration_from))
+        if self.duration_from:
+            filters.append(tour.duration >= self.duration_from)
 
         # Проверка параметра duration_to
-        if self.duration_to is not None:
-            filters.append(tour.duration <= int(self.duration_to))
+        if self.duration_to:
+            filters.append(tour.duration <= self.duration_to)
 
         #Проверка параметра category_id
         # if self.category_id is not None:
         #     filters.append(self.category_id in tour.category_ids)
-        if self.category_id is not None:
-            filters.append(int(self.category_id) == tour.category_id)
+        if self.category_id:
+            filters.append(self.category_id == tour.category_id)
 
-        if self.type_id is not None:
-            filters.append(int(self.type_id) == tour.main_type_id)
+        if self.type_id:
+            filters.append(self.type_id == tour.main_type_id)
 
-        if self.accommodation_id is not None:
-            filters.append(int(self.accommodation_id) in tour.accommodation_ids)
+        if self.accommodation_id:
+            filters.append(self.accommodation_id in tour.accommodation_ids)
 
-        if self.age_group_from is not None:
-            filters.append(tour.age_group_from >= int(self.age_group_from))
+        if self.age_group_from:
+            filters.append(tour.age_group_from >= self.age_group_from)
 
         # Проверка параметра age_group_to
-        if self.age_group_to is not None:
-            filters.append(tour.age_group_to <= int(self.age_group_to))
+        if self.age_group_to:
+            filters.append(tour.age_group_to <= self.age_group_to)
         # Проверка параметра is_one_day_tour
-        if self.is_one_day_tour is not None:
+        if self.is_one_day_tour:
             filters.append(tour.is_one_day_tour == self.is_one_day_tour)
 
         # Проверка параметра children_age_id
-        if self.children_age_id is not None:
-            filters.append(tour.children_age_id == int(self.children_age_id))
+        if self.children_age_id:
+            filters.append(tour.children_age_id == self.children_age_id)
 
         # Проверка параметра activity_level_id
-        if self.activity_level_id is not None:
-            filters.append(tour.activity_level_id == int(self.activity_level_id))
+        if self.activity_level_id:
+            filters.append(tour.activity_level_id == self.activity_level_id)
 
-        if self.language_id is not None:
-            filters.append(int(self.language_id) in tour.language_ids)
+        if self.language_id:
+            filters.append(self.language_id in tour.language_ids)
 
-        if self.currency_id is not None:
+        if self.currency_id:
             price_filters = []
             
             for tour_price in tour.prices:
-                if self.is_price_for_day is not None:
+                if self.is_price_for_day:
                     price_filters.append(
-                        int(self.currency_id) == tour_price.currency_id and
-                        float(self.price_from) <= tour_price.price_for_day <= float(self.price_to)
+                        self.currency_id == tour_price.currency_id and
+                        self.price_from <= tour_price.price_for_day <= self.price_to
                     )
                 else:
                     price_filters.append(
-                        int(self.currency_id) == tour_price.currency_id and
-                        float(self.price_from) <= tour_price.price <= float(self.price_to)
+                        self.currency_id == tour_price.currency_id and
+                        self.price_from <= tour_price.price <= self.price_to
                     )
             
             # Use any() to check if at least one price filter condition is satisfied
