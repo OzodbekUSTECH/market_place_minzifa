9221beee747ee5c3b2d0fa20c5487aa8dc584185
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-10-16 21:41:29 +0500

    updated filter tours

--- utils/filters/filter_tours.py
+++ utils/filters/filter_tours.py
@@ -1,5 +1,4 @@
 from fastapi import Query
-from pydantic import Field
 from fuzzywuzzy import fuzz
 import models
 from utils.locale_handler import LocaleHandler
@@ -9,7 +8,7 @@ from utils.filters.base import BaseFilterParams
 class FilterToursParams(BaseFilterParams):
     def __init__(
         self,
-        country_id: int | None = Field(None),
+        country_id: int | None = Query(None),
         region_id: int | None= Query(None),
         start_month: int | None= Query(None),
         query: str | None= Query(None),
