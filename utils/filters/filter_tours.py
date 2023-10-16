17b940cbefad7d4b946dd6b65b3e8663d1255710
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-10-16 23:24:09 +0500

    updated filter tours

--- utils/filters/filter_tours.py
+++ utils/filters/filter_tours.py
@@ -11,7 +11,7 @@ class FilterToursParams(BaseFilterParams):
         country_id: int = Query(0),
         region_id: int = Query(0),
         start_month: int = Query(0),
-        query: int | str = Query(0),
+        query: str = Query(0),
         user_id: int = Query(0),
         rating: int | float = Query(0),
         status_id: int = Query(0),
@@ -80,7 +80,7 @@ class FilterToursParams(BaseFilterParams):
             filters.append(self.start_month == tour.start_month)
 
         # Проверка параметра query
-        if self.query:
+        if self.query != 0:
             filters.append(
                 fuzz.partial_ratio(
                     self.query.lower(), tour.title[locale.get_language].lower()
